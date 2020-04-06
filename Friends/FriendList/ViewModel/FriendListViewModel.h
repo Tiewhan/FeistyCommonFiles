26adfb7ec2accd5560054898dd5cf40d01527103
Tiewhan Smith <TSmith@jhb.dvt.co.za>
2020-04-06 12:03:19 +0200

    Add Friend Structure and View Model

---
+++ Friends/FriendList/ViewModel/FriendListViewModel.h
@@ -0,0 +1,16 @@
+//
+//  FriendListViewModel.h
+//  CommonFiles
+//
+//  Created by Tiewhan Smith on 2020/04/06.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FriendListViewModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
