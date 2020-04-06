9b70d293189f2ab4c11058569032e43f7797c2cc
Tiewhan Smith <TSmith@jhb.dvt.co.za>
2020-04-06 16:19:56 +0200

    Update repo to better handle requests and make requests

--- Friends/FriendList/Model/FriendListModel.swift
+++ Friends/FriendList/Model/FriendListModel.swift
@@ -10,21 +10,64 @@ import Foundation
 
 public class FriendListModel {
   
-  private let viewModel: FriendListViewModelType
+  public var observers: [String : FriendListModelObserver] = [:]
   
-  init(withViewModel viewModel: FriendListViewModelType) {
-    self.viewModel = viewModel
+  private let repo: FriendListRepositoryType
+  private var friendList: [User] = []
+  
+  public init(withRepo repo: FriendListRepositoryType) {
+    self.repo = repo
+    
+    self.repo.subscribeToFriendListModel(with: self, andID: observerID)
+    
   }
   
 }
 
 extension FriendListModel: FriendListModelType {
   
-  public func friendListFound(withData friends: [User]) {
-    viewModel.friendListFound(withData: friends)
+  public func getFriend(at index: Int) -> User {
+    return friendList[index]
+  }
+    public func subscribeToFriendListModel(with subscriber: FriendListModelObserver, andID observerID: String) {
+    observers[observerID] = subscriber
+  }
+  
+  public func unsubscribeFromFriendListModel(withID observerID: String) {
+    observers.removeValue(forKey: observerID)
   }
   
   public func getFriendList() {
+    repo.getFriendListData(using: ServiceCaller())
+  }
+  
+  public func getAmountOfFriends() -> Int {
+    return friendList.count
+  }
+  
+}
+
+extension FriendListModel: FriendListRepoObserver {
+  
+  public var observerID: String {
+    return "FriendListModelObserver"
+  }
+  
+  public func friendsListRetrieved(withData data: [User]) {
+    
+    friendList = data
+    
+    observers.forEach { observer in
+      observer.value.friendListFound()
+    }
+    
+  }
+  
+  public func failedToLoadFriends() {
+    
+    observers.forEach { observer in
+      observer.value.friendListNotFound()
+    }
     
   }
   
